0e996550758d19de8d6ddb507e9ffe674917b7f4
Vishal Chaphekar <Vishal>
2024-07-12 08:49:14 -0500

    updated the build spec moved file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello <code>src/App.js</code> from the Platform Engineering team.
+          Hello <code>src/App.js</code> from the Platform Engineering Team.
         </p>
         <a
           className="App-link"
